43e9242f2548776dc6380412a55a19087af2f285
edsUlalan <edds.ulalan@gmail.com>
2021-08-10 08:29:13 +0800

    my first program using C++

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //My first C++ program
+  cout << "Hello, world!\n";
+  cout << "And greetings from C++";
+
+  /* This comment can
+   * cover several lines
+   * without the need to add more slashes */
+
+  cout << "This line is now being ignored by the compiler!\n"
+}
