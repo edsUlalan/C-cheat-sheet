6aa1e31265dd1a923ff2c4143bab1170ce46edea
edsUlalan <edds.ulalan@gmail.com>
2021-08-10 08:51:42 +0800

    add examples in variable declaration

---
+++ variables.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int StartLives = 3; // this is a  global variable
+auto pi = 3.141593;
+
+int main()
+
+{
+  double area, radius = 1.5;
+  startLives = StartLives - 1
+  int a;
+  int b;
+  int result;
+
+  a = 10;
+  b = 5;
+  result = a + b;
+  area = pi * radius * radius;
+
+  // you can assign varaibles like this
+  int c (2);
+  int d (3);
+  int product (c+d);
+
+  cout << a;
+  cout << "\n";
+  cout << b;
+  cout << result;
+  cout << product;
+  cout << startLives;
+  cout << area;
+}
