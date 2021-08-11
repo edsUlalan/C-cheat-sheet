434c2aad4a12a7147fd82b753fb2effbecd6f849
edsUlalan <edds.ulalan@gmail.com>
2021-08-11 22:13:35 +0800

    add datatype example and customization

---
+++ datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+double PI = 3.141592653589793238463;
+typedef int metres; // create own datatype
+
+int main()
+{
+  cout.precision(22);
+  cout << PI;
+
+  metres distance;
+  distance = 15;
+  cout << "distance in metres is" << distance;
+}
