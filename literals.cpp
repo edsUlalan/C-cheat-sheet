ae787e06727b67cb26b947fac45438a25d4fffa5
edsUlalan <edds.ulalan@gmail.com>
2021-08-13 22:29:32 +0800

    add  examples on string literals

---
+++ literals.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello, user. This is how to use \"quotes".";
+  cout << "ALARM! \a"; // activate sound alarm
+  cout << "\u00A9"; // unicode character
+}
