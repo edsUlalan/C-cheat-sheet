826a898968cb6545c126c80453eeee250b4779e6
edsUlalan <edds.ulalan@gmail.com>
2021-08-16 22:18:59 +0800

    add reference for character literals

--- literals.cpp
+++ literals.cpp
@@ -7,3 +7,18 @@ int main ()
   cout << "ALARM! \a"; // activate sound alarm
   cout << "\u00A9"; // unicode character
 }
+
+// Escape Sequence Code - character
+// \\ - Backlash
+// /' - Single quote
+// \" - Double quote (Speech Marks)
+// \? - Question mark
+// \a - Alert/Alarm
+// \b - Backspace
+// \f - Form feed
+// \r - Carriage return
+// \t - Horizontal Tab
+// \v - Vertical Tab
+// \0 - Null character
+// \uxxxx - Unicode (UTF-8)
+// \Uxxxxxxxx - Unicode (UTF-16)
