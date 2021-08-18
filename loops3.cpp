3a5731d8cca042df60eacbdfef395b300a31f5fa
edsUlalan <edds.ulalan@gmail.com>
2021-08-18 12:49:05 +0800

    add examples using for loops

---
+++ loops3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  // For Loop Begins
+  for( int num = 1; num < 30; num = num + 1)
+  {
+    cout << "Number: " << num << endl;
+  }
+  return 0;
+}
