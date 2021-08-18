9a257b17657256557255729c7f5ca9965a029018
edsUlalan <edds.ulalan@gmail.com>
2021-08-18 12:33:44 +0800

    add more example to while loops

--- loops.cpp
+++ loops.cpp
@@ -10,4 +10,13 @@ int main ()
     num = num + 1;
   }
   return 0;
+
+  // if you don't want ti see the continuous increase in number
+  int number = 0;
+  while (num < 30)
+  {
+    num++;
+    cout << "Number: " << num << endl;
+  }
+  return 0;
 }
