6faa0665758a72a93d69269378082774797ac1eb
edsUlalan <edds.ulalan@gmail.com>
2021-08-20 13:31:01 +0800

    add more examples in using for loops

--- loops3.cpp
+++ loops3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main ()
@@ -8,5 +9,25 @@ int main ()
   {
     cout << "Number: " << num << endl;
   }
+  return 0;
+
+  // create a 10 second countdown
+  for(int a = 10; a != 0; a = a - 1)
+  {
+    cout << a << endl;
+    Sleep(1000);
+  }
+  cout << "\nBlast Off!" << endl;
+  return 0;
+
+  // include user input
+  int i, n, fact = 1;
+
+  cout << "Enter a whole number: ";
+  cin >> n;
+  for (i = 1; i <= n; ++i) {
+    fact *= i;
+  }
+  cout << "\nFactorial of "<< n <<" = "<< fact << endl;
   return 0;
 }
