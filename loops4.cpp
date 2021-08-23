797bb15348c81bf4d252c476026bc5e1cddc3dd0
edsUlalan <edds.ulalan@gmail.com>
2021-08-23 08:59:53 +0800

    add examples for do while loop

---
+++ loops4.cpp
@@ -0,0 +1,14 @@
+// do... while loops
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int num = 1;
+  do {
+    cout << "Number: " << num << endl;
+    num = num + 1;
+  }
+  while (num < 30);
+  return 0;
+}
