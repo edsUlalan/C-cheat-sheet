682d2e052dcfdf0f076921d1ac6faddf0ecec5b6
edsUlalan <edds.ulalan@gmail.com>
2021-08-24 10:32:57 +0800

    no changes

---
+++ conditionals.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  int num = 1;
+  if (num < 30) {
+    cout << "The number is less than 30." << endl;
+  }
+  cout << "Value of a number is: " << num << endl;
+  return 0;
+}
