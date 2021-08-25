12e9b2a48dfb65ffb94dec407ad09874cba77b5f
edsUlalan <edds.ulalan@gmail.com>
2021-08-25 14:06:52 +0800

    add example on if statement inside do while loop

--- conditionals.cpp
+++ conditionals.cpp
@@ -8,4 +8,22 @@ int main () {
   }
   cout << "Value of a number is: " << num << endl;
   return 0;
+
+  // include if statement within a do... while loop
+  float temp;
+  do {
+    cout << "\nEnter the temperature (or -10000 to exit): " << endl;
+    cin >> temp;
+    if (temp <= 0)
+    {
+      cout << "\nBrrrr, it's really cold!" << endl;
+    }
+    if (temp > 0)
+    {
+      cout << "\nAt least it's not freezing!" << endl;
+    }
+    while (temp != -10000);
+    cout << "\nGood bye\n" << endl;
+    return 0;
+  }
 }
